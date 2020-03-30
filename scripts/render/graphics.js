590aba8d0b9ad2804fec1025ba552e1387cbce44
Carson <carsonwsorenson@gmail.com>
2020-03-30 00:26:09 -0600

    fixed bg image

--- scripts/render/graphics.js
+++ scripts/render/graphics.js
@@ -46,8 +46,8 @@ MyGame.graphics = (function(assets) {
     }
 
     function resize() {
-        let width = window.innerWidth * 0.99;
-        let height = window.innerHeight * 0.99;
+        let width = window.innerWidth * 0.9;
+        let height = window.innerHeight * 0.9;
         let ratio = 1.69;
 
         if (width / height < ratio) {
